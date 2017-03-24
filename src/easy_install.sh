b302caaf522d2748e0d397a60aec2010591bfbed
Benjamin Pachev <benjaminpachev@gmail.com>
2017-03-24 11:27:00 -0600

    Added easy install script.

---
+++ src/easy_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+export version=$1
+export module_name=xgboost
+./setup $version
+
+make clean
+make
+sudo make install
+
+function find_mods_available_dir
+{
+    local version=$1
+    local cmd=php
+    if [ "$1" ]; then
+        cmd=php$1
+    fi
+    dirname $(dirname $($cmd -r 'echo(php_ini_loaded_file());'))
+}
+
+export mods_available_dir=$(find_mods_available_dir $version)/mods-available
+echo "extension=${module_name}.so" | tee $mods_available_dir/$module_name.ini
+if [ "$version" = "5" ]; then
+    php5enmod $module_name
+elif [ "$version" ]; then
+    phpenmod -v $version $module_name
+else
+    phpenmod $module_name
+fi
